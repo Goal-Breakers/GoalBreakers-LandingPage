75fb719e23ea710566ef2423a6a6b4155e648fbe
Eduardo Ulisses <eduardoulisses2006@gmail.com>
2025-09-13 18:02:21 -0300

    feat(pageNotFound) add PageNotFound page

--- goalbreakers-web/src/routes/AppRoutes.jsx
+++ goalbreakers-web/src/routes/AppRoutes.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../pages/App";
+import PageNotFound from "../pages/PageNotFound";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App/>,
+        errorElement: <PageNotFound/>
     }
 ])
