254a74dde6f6a16f713ef924ceee3384b62e8219
Eduardo Ulisses <eduardoulisses2006@gmail.com>
2025-09-16 00:12:44 -0300

    fix(copinha) fix copinha route for not admins

--- goalbreakers-web/src/routes/ProtectedRoute.jsx
+++ goalbreakers-web/src/routes/ProtectedRoute.jsx
@@ -2,12 +2,9 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function ProtectedRoute({ children }) {
-  const { user, isAdmin } = useAuth();
+  const { user } = useAuth();
   if (!user) {
     return <Navigate to="/login" replace />;
-  }
-  if (!isAdmin()) {
-    return <Navigate to="/" replace />;
   }
   return children;
 }
